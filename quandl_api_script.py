588872a2e43022cd02eda0a4709c9744131b0dc7
Chris Luiz <chris@chrisluiz.io>
2016-07-19 13:01:16 -0600

    quandl script done

--- quandl_api_script.py
+++ quandl_api_script.py
@@ -21,38 +21,43 @@ def get_file_metadata(zip_url):
     file_list = []
     zipfile = ZipFile(StringIO(urlopen(zip_url).read()))
     names = zipfile.namelist()
+    dir_prefix = zip_url.split('/')[-2]
     for name in names:
         for line in zipfile.open(name).readlines():
             file_list.append(line.strip('\n').split(','))
-    return file_list
+    return file_list, dir_prefix
 # list comp ^^
 # file_list = [line for name in names for line in zipfile.open(name).readlines()...]
 
-def get_files(file_names, target_directory_name):
+def get_files(file_list, dir_prefix, target_directory_name):
     '''INPUT: list of file names, target directory name
        OUTPUT: csv files of passed names in target directory
     '''
-    path = os.getcwd()+'/{}/'.format(target_directory_name)
+    path = os.getcwd()+'/{}/{}/'.format(target_directory_name, dir_prefix)
     ql.ApiConfig.api_key = quandl_api_key
 
     if not os.path.exists(path):
         os.makedirs(path)
 
-    for file_name in file_names:
+    for file_name in file_list:
         fixed_file_name = file_name[1].lower().replace(' - ', '_').replace(' ', '_')
-        file_path = '{}{}{}'.format(path, fixed_file_name, '.csv')
+        file_path = '{}{}.csv'.format(path, fixed_file_name)
         if not os.path.isfile(file_path):
             ql.get(file_name[0]).to_csv(file_path)
 
-def quandl_data(lst):
+def quandl_data(db_codes, target_directory_name='data'):
+    '''INPUT: List of target quandl db codes
+       OUTPUT: Csv's from targeted db's nested in the 'data' directory '''
+    urls=[]
+    url_prefix = 'https://www.quandl.com/api/v3/databases/'
+
+    for code in db_codes:
+        urls.append(('{}{}/codes').format(url_prefix, code))
+
     for url in urls:
-        file_names, dir_prefix = get_file_metadata(url)
-        get_files(file_names, dir_prefix, 'data/')
+        file_list, dir_prefix = get_file_metadata(url)
+        get_files(file_list, dir_prefix, target_directory_name)
 
-if __name__ == '__main__':
-    urls = ['https://www.quandl.com/api/v3/databases/NVCA/codes',
-'https://www.quandl.com/api/v3/databases/RENCAP/codes',
-'https://www.quandl.com/api/v3/databases/CVR/codes',
-'https://www.quandl.com/api/v3/databases/COOLEY/codes']
-
-    quandl_data(urls)
+# if __name__ == '__main__':
+#     db_codes = ['NVCA', 'RENCAP', 'CVR', 'COOLEY']
+#     quandl_data(db_codes)
