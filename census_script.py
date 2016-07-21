77f4082a6672561deaa02c7db10165582b2b6d7d
Chris Luiz <chris@chrisluiz.io>
2016-07-21 15:33:24 -0600

    population data eda

---
+++ census_script.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+
+
+url = 'http://www.census.gov/data/datasets/1997/econ/susb/1997-susb.html'
+
+doc=requests.get(url).text
+
+soup = BeautifulSoup(doc)
+
+print soup.prettify()
+
+sooup_links = soup.findAll('a', href=re.compile('^www2.census.gov/programs-surveys/susb/datasets/1997'))
+
+links = re.findall("www2.census.gov/programs-surveys/susb/datasets/1997", str(doc))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# http://www2.census.gov/programs-surveys/susb/datasets/1997/msa_2digitsic_1997.txt
+#
+# http://www2.census.gov/programs-surveys/susb/datasets/1997/us_state_4digitsic_1997.txt
+# http://www2.census.gov/programs-surveys/susb/datasets/1997/us_state_sicmajorindustry_detailedsizes_1997.txt
+#
+#
+# http://www.census.gov/data/datasets/2012/econ/susb/2012-susb.html
+#
+# http://www2.census.gov/programs-surveys/susb/datasets/2012/us_state_6digitnaics_2012.txt
+#
+# http://www2.census.gov/programs-surveys/susb/datasets/2012/us_state_naics_detailedsizes_2012.txt
