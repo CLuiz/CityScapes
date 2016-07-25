b411ad6b4f22155e88c88cce312e9b6c3d0326de
Chris Luiz <chris@chrisluiz.io>
2016-07-25 17:22:03 -0600

    bea datwa claenaed and integrated

--- population_cleanup.py
+++ population_cleanup.py
@@ -1,7 +1,10 @@
+from __future__ import division
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+
 
 df = pd.read_csv('data/1790-2010_MASTER.csv')
 df.set_index(['City'], inplace=True)
@@ -22,3 +25,16 @@ rj_df.set_index(['city'], inplace = True)
 new_df = pd.concat([df, rj_df], axis=1)
 meetup_df = new_df[new_df['Pop'].notnull()]
 cities = list(meetup_df.index)
+
+# buruea of economic affiars clean and join
+bea_df = pd.read_csv('data/bea.csv', header=2)
+columns = bea_df.columns
+new_columns = [u'city', u'bea_state', u'bea_2009', u'bea_2010', u'bea_2011', u'bea_2012', u'bea_2013', u'bea_2014', u'bea_what_is_this _crap']
+bea_df.columns = new_columns
+#bea_df = bea_df[1:]
+bea_df['city'] = bea_df['bea_state'].apply(lambda x: x.split(',')[0])
+bea_df['city'] = bea_df['city'].apply(lambda x: x.lower().replace('-', '_').replace(' ', '_'))
+bea_df['bea_state'] = bea_df['bea_state'].apply(lambda x: x.split(',')[-1])
+bea_df.set_index('city', inplace=True)
+
+next_df = pd.concat([new_df, bea_df], axis=0)
