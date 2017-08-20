b03196ed7b949c347c7e48964596bcc143974e78
Chris Luiz <chris@chrisluiz.io>
2017-08-19 21:09:30 -0400

    linting

--- city_test_script.py
+++ city_test_script.py
@@ -1,22 +1,22 @@
-import requests
-from bs4 import BeautifulSoup
 import os
-import pandas as pd
 
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
 
 
 def run_me():
     df1 = pd.read_csv('data/biggestuscities/business_retail_sales_per_capita_by_top_100_city.csv')
-    df1['city']=df1['city'].apply(lambda x: x.replace('_', '-'))
-    df1['state_rs']=df1['state_rs'].apply(lambda x: x.replace('_', '-'))
-    df1['combo'] = df1['city']+'-'+df1['state_rs']
+    df1['city'] = df1['city'].apply(lambda x: x.replace('_', '-'))
+    df1['state_rs'] = df1['state_rs'].apply(lambda x: x.replace('_', '-'))
+    df1['combo'] = df1['city'] + '-' + df1['state_rs']
     city_list = list(df1['combo'])
-    #city_list =['cleveland-ohio', 'atlanta-georgia', ]
+    #city_list = ['cleveland-ohio', 'atlanta-georgia', ]
     for city in city_list:
         url_suffix = city
         url_prefix = 'https://www.biggestuscities.com/city/'
         url = url_prefix + url_suffix
-        path = os.getcwd()+'/data/biggestuscities'
+        path = os.getcwd() + '/data/biggestuscities'
         file_name = url.split('/')[-1].replace('-', '_')
         file_path = '{}/{}.csv'.format(path, file_name)
         doc = requests.get(url)
@@ -24,13 +24,13 @@ def run_me():
             soup = BeautifulSoup(doc.content, 'lxml')
             table = soup.findAll('table')
             # print table
-            tabs =[tag.text for tag in table]
+            tabs = [tag.text for tag in table]
             # print tabs
-            data =[t.replace("\n",",").strip() for t in tabs][-1].split(',,')[1:-1]
+            data = [t.replace("\n",",").strip() for t in tabs][-1].split(',,')[1:-1]
             # print data
             data = [item[1:].split(',') for item in data if item]
-            dd= pd.DataFrame(data)[1:6]
-            dd['pop']=dd[1]+dd[2]
-            da =pd.DataFrame(zip(dd[0], dd['pop']))
+            dd = pd.DataFrame(data)[1:6]
+            dd['pop'] = dd[1]+dd[2]
+            da = pd.DataFrame(zip(dd[0], dd['pop']))
             done = da.T
     return done
